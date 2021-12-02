c7842142e6a4d3f77a5420fc0947279b1d5bc12b
quan96it.sami.hust.edu <quan96it.sami.hust.edu@gmail.com>
2021-12-02 12:21:14 +0700

    frist commit

---
+++ quân mã đi tuần_bản chuẩn/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace quân_mã_đi_tuần_bản_chuẩn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_vitrixuatphat = new System.Windows.Forms.TextBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_speed = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chương trình thuật toán quân mã đi tuần";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nhập ô xuất phát";
+            // 
+            // txt_vitrixuatphat
+            // 
+            this.txt_vitrixuatphat.Location = new System.Drawing.Point(194, 95);
+            this.txt_vitrixuatphat.Name = "txt_vitrixuatphat";
+            this.txt_vitrixuatphat.Size = new System.Drawing.Size(105, 20);
+            this.txt_vitrixuatphat.TabIndex = 2;
+            this.txt_vitrixuatphat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(28, 120);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(446, 32);
+            this.btn_run.TabIndex = 3;
+            this.btn_run.Text = "Chạy chương trình";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(28, 158);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 379);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(322, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Speed";
+            // 
+            // txt_speed
+            // 
+            this.txt_speed.Location = new System.Drawing.Point(383, 95);
+            this.txt_speed.Name = "txt_speed";
+            this.txt_speed.Size = new System.Drawing.Size(66, 20);
+            this.txt_speed.TabIndex = 6;
+            this.txt_speed.Text = "10";
+            this.txt_speed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_speed.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_speed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 570);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_speed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.txt_vitrixuatphat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_vitrixuatphat;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_speed;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ quân mã đi tuần_bản chuẩn/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace quân_mã_đi_tuần_bản_chuẩn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            draw_chessboard();
+        }
+        private List<List<Button>> chessboard;
+        private List<List<int>> nhan_chessboard;
+        private int cot = 8;
+        private void  draw_chessboard()
+        {
+            chessboard = new List<List<Button>>();
+            nhan_chessboard = new List<List<int>>();
+            Button btnn = new Button()
+            {
+                Width = 40,
+                Height = 40,
+                Location = new Point(60, 30),
+                FlatStyle=FlatStyle.Flat,
+            };
+            for(int i=0;i<cot;i++)
+            {
+                chessboard.Add(new List<Button>());
+                nhan_chessboard.Add(new List<int>());
+                for (int j=0;j< cot; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = btnn.Width,
+                        Height = btnn.Height,
+                        Location = new Point(btnn.Location.X + btnn.Width * j, btnn.Location.Y + btnn.Height * i),
+                        Tag = i,
+                        FlatStyle = FlatStyle.Flat,
+                    };
+                    panel1.Controls.Add(btn);
+                    chessboard[i].Add(btn);
+                    nhan_chessboard[i].Add(0);
+                    btn.Click += Btn_Click;
+                }
+
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            Point p = location_button(btn);
+            txt_vitrixuatphat.Text = "(" + p.X + "," + p.Y + ")";
+        }
+
+        private Point location_button(Button btn)
+        {
+            int location_X = Convert.ToInt32(btn.Tag);
+            int location_Y = chessboard[location_X].IndexOf(btn);
+            return new Point(location_X,location_Y);
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private int speed = 10;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                speed = int.Parse(txt_speed.Text);
+            }
+            catch { speed = 10; }
+        }
+        private int start_locationX = 0;
+        private int start_locationY = 0;
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = txt_vitrixuatphat.Text;
+                string[] data = s.Split('.', ',', '(', ')', ' ');
+                int dem = 0;
+                for(int i=0;i<data.Count();i++)
+                {
+                    if(data[i]!="")
+                    {
+                        dem++;
+                        if(dem==1)
+                        {
+                            start_locationX = int.Parse(data[i]);
+                        }
+                        if (dem == 2)
+                        {
+                            start_locationY = int.Parse(data[i]);
+                        }
+                    }
+                }
+                nhan_chessboard[start_locationX][start_locationY] = 1;   // vi xuat phat tai vi tri nay nen mac dinh nhan =1 ko duoc gan vao nua
+                chessboard[start_locationX][start_locationY].Text = "1";
+                Thread ts = new Thread(() => Try(start_locationX, start_locationY, 1));
+                ts.Start();
+                
+            }
+            catch { }
+        }
+        #region --- thuật toán đệ qui cho bài toán quân mã đi tuần ----
+
+        private int[] X = new int[] { 2, 2, 1, -1, -2, -2, -1, 1 };
+        private int[] Y = new int[] { 1, -1, -2, -2, -1, 1, 2, 2 };
+        private void Try(int x,int y,int dem)
+        {
+            int xthe = 0;
+            int ythe = 0;
+            for(int i=0;i< 8; i++)
+            {
+                xthe = x + X[i];
+                ythe = y + Y[i];
+                if (xthe < cot && ythe < cot && xthe >= 0 && ythe >= 0)
+                {
+                    if (nhan_chessboard[xthe][ythe] == 0)
+                    {
+                        chessboard[xthe][ythe].Text = (dem+1).ToString();
+                        nhan_chessboard[xthe][ythe] = 1;
+                        Thread.Sleep(speed);
+                        if (dem == cot*cot)
+                        {
+                            MessageBox.Show("ok");
+                            chessboard[xthe][ythe].Text = "";
+                            nhan_chessboard[xthe][ythe] = 0;
+                            break;
+                        }
+                        else
+                        {
+                            Try(xthe, ythe, dem + 1);
+                            chessboard[xthe][ythe].Text = "";
+                            nhan_chessboard[xthe][ythe] = 0;
+                            Thread.Sleep(speed);
+                        }
+                        
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
